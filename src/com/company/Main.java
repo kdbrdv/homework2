e7c0bd0e900c4be610f7dcf28fddcf65daef8f75
kdbrdv <kudaiberdiev24u@gmail.com>
2021-02-24 19:19:37 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ageOfPerson = 10;
+        int outsideTemperature = 18;
+
+        if (ageOfPerson < 20) ;
+        System.out.println("Можно идти гулять");
+        if (ageOfPerson < 20 && outsideTemperature 0);
+        System.out.println("Можно идти гулять");
+        if (ageOfPerson < 45);
+        System.out.println("Можно идти гулять");
+
+
+    }
+}
